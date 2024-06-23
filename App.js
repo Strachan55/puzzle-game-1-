bdd82e991fbbb0be35a0e6676f264503fe7b4083
Strachan55 <strachan55@yahoo.com>
2024-06-23 22:53:43 +0700

    Create App.js
    this is a mother test

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PuzzleGame1 from './PuzzleGame1';
+
+function App() {
+  return (
+    <div className="App">
+      <PuzzleGame1 />
+    </div>
+  );
+}
+
+export default App;
